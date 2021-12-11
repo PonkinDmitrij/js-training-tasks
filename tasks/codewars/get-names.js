513d9817b96a5c65023ed1128f470690e9ac0105
Dmitrij Ponkin <ponkindmitrij@gmail.com>
2021-12-11 19:35:14 +0300

    Add task "Get names" (codewars)

---
+++ tasks/codewars/get-names.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+/**
+ * Get names.
+ *
+ * https://www.codewars.com/kata/514a677421607afc99000002
+ *
+ * $tag bugs
+ *
+ * @param {object[]} arr - Array of objects.
+ *
+ * @returns {string[]} Array of string.
+ */
+const getNames = (arr) => arr.map((user) => user.name);
+
+export { getNames };
