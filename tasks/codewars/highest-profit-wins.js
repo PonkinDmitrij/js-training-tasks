047a8f41a9d9d7135b8bfbf8d6d3f0f99e21cca0
Dmitrij Ponkin <ponkindmitrij@gmail.com>
2022-03-12 12:38:55 +0300

    Add task "The highest profit wins!" (codewars)

---
+++ tasks/codewars/highest-profit-wins.js
@@ -0,0 +1,8 @@
+// The highest profit wins!
+// https://www.codewars.com/kata/559590633066759614000063
+
+const minMax = (arr) => {
+  return [Math.min(...arr), Math.max(...arr)];
+};
+
+export { minMax };
